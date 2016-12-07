a7a11334a522dd7bc4bc3a34da41bd8889cb3f8f
chuqingq <chuqingq@qq.com>
2016-12-07 22:32:32 +0800

    封装几个内部使用的函数

--- python_uiautomator/main3.py
+++ python_uiautomator/main3.py
@@ -2,6 +2,7 @@
 
 """
 操作步骤：
+* 需要先打开三星手机上的insecureADB
 * 修改设备串号和adb端口：bbb5fc231f5c3，5037
 * 指定安卓用户：u0_a121
 * 指定微信的控件ID：你的手机号码，填写密码
@@ -12,49 +13,17 @@ import time
 import os
 from uiautomator import Device
 
-import data
+from data import accounts
 
 def log(str):
     print(time.strftime('%Y-%m-%d %H:%M:%S') + ': ' + str)
 
-log('connect to device...')
 # d = Device('071efe2c00e37e37', adb_server_host='127.0.0.1', adb_server_port=5037) # nexus 5 home
 # d = Device('bbb5fc231f5c3', adb_server_port=5037) # redmi 4a 1
-d = Device(data.imei, adb_server_port=5037) # 三星galaxy E7
+d = Device('200ac4ae', adb_server_port=5037) # 三星galaxy E7
+user = 'u0_a140' # 手机上微信的操作系统用户
 
-accounts = data.accounts
-
-def registerOne(account, password):
-    log('register ' + account + '...')
-    log('rm...')
-    os.system('adb shell rm -rf /data/data/com.tencent.mm')
-    os.system('adb shell rm -rf /mnt/sdcard/tencent')
-    log('mkdir...')
-    os.system('adb shell mkdir -p /data/data/com.tencent.mm')
-    os.system('adb shell mkdir -p /mnt/sdcard/tencent')
-    log('ln...')
-    os.system('adb shell ln -s /data/app/com.tencent.mm-1/lib/arm /data/data/com.tencent.mm/lib')
-    log('chown...')
-    os.system('adb shell chown '+data.user+':'+data.user+' /data/data/com.tencent.mm')
-    log('am start...')
-    os.system('adb shell am start -n com.tencent.mm/com.tencent.mm.ui.LauncherUI')
-    # 点击注册按钮
-    d(text=u'注册').wait.exists()
-    d(text=u'注册').click()
-    # 输入昵称
-    d(text=u'例如：陈晨').set_text(account)
-    # 输入手机号
-    d(text=u'你的手机号').click()
-    inputAccount(account)
-    # 输入密码
-    d(resourceId='com.tencent.mm:id/fo').set_text(password) # TODO 资源ID可能不是这个
-    pass # TODO 后续流程
-
-def registerAll():
-    for (n, p) in accounts.items():
-        resgisterOne(n, p)
-
-registerAll()
+## ---- 内部函数
 
 # 输入账号，因为微信会针对11位手机号自动分成3、4、4中间有空格，所以需要一段一段输入
 def inputAccount(account):
@@ -62,8 +31,11 @@ def inputAccount(account):
     os.system('adb shell input text ' + account[3:7])
     os.system('adb shell input text ' + account[7:])
 
-def loginOne(account, password):
-    log('login ' + account + '...')
+
+def clearDataAndStart():
+    log('clearDataAndStart')
+    os.system('adb shell am force-stop com.tencent.mm HERE')
+    # 创建空环境
     log('rm...')
     os.system('adb shell rm -rf /data/data/com.tencent.mm')
     os.system('adb shell rm -rf /mnt/sdcard/tencent')
@@ -73,9 +45,74 @@ def loginOne(account, password):
     log('ln...')
     os.system('adb shell ln -s /data/app/com.tencent.mm-1/lib/arm /data/data/com.tencent.mm/lib')
     log('chown...')
-    os.system('adb shell chown '+data.user+':'+data.user+' /data/data/com.tencent.mm')
+    os.system('adb shell chown '+user+':'+user+' /data/data/com.tencent.mm')
     log('am start...')
     os.system('adb shell am start -n com.tencent.mm/com.tencent.mm.ui.LauncherUI')
+
+def restore(account):
+    log('restore '+account)
+    # 移动号码的目录到/data/data/com.tencent.mm
+    os.system('adb shell am force-stop com.tencent.mm HERE')
+    os.system('adb shell mv /data/data/com.tencent.mm.'+account+' /data/data/com.tencent.mm')
+    os.system('adb shell mv /mnt/sdcard/tencent.'+account+' /mnt/sdcard/tencent')
+
+def backup(account):
+    log('backup '+account)
+    os.system('adb shell am force-stop com.tencent.mm HERE')
+    os.system('adb shell mv /data/data/com.tencent.mm /data/data/com.tencent.mm.'+account)
+    os.system('adb shell mv /mnt/sdcard/tencent /mnt/sdcard/tencent.'+account)
+
+## ---- 业务逻辑
+def register(account, password):
+    log('register ' + account + '...')
+    clearDataAndStart()
+    # 点击注册按钮
+    d(text=u'注册').wait.exists()
+    d(text=u'注册').click()
+    # 应该有4个EditText
+    editTextSelector = d(className='android.widget.EditText')
+    if editTextSelector.count != 4:
+        log('!!!ERROR editTextSelector.count should be 4...')
+        return
+    nickSelector = editTextSelector[0]
+    phoneSelector = editTextSelector[2]
+    passwordSelector = editTextSelector[3]
+    # 输入昵称
+    nickSelector.set_text(account)
+    # 输入手机号
+    phoneSelector.click()
+    inputAccount(account)
+    # 输入密码
+    passwordSelector.click()
+    passwordSelector.set_text(password)
+    # 点击注册
+    d(text=u'注册').click()
+    # 点击“确定”（发送验证码到手机）
+    d(text=u'确定').wait.exists()
+    d(text=u'确定').click()
+    # 人工接收验证码，并输入，点击下一步
+    raw_input('输入验证码后请点击回车：')
+    # 点击“好”（查找你的微信朋友） 如果手机号已绑定，则不会有“好”。建议手工操作
+    # d(text=u'好').wait.exists()
+    # d(text=u'好').click()
+    # 备份会话
+    log('wait for "搜索" exists...')
+    while not d(description=u'搜索').exists: # TODO 有可能判断的时候还不存在
+        if d(text=u'否').exists:
+            d(text=u'否').click.wait() # 不使用通讯录（不是每次都有）
+        time.sleep(0.1)
+    time.sleep(2)
+    backup(account)
+    log('regsiter ' + account + ' success')
+
+def registerAll():
+    for (n, p) in accounts.items():
+        register(n, p)
+
+
+def login(account, password):
+    log('login ' + account + '...')
+    clearDataAndStart()
     # 如果已有账号，则点击“更多”到输入账号页面；否则，点击登录，才能输入账号
     log('wait for "登录" exists...')
     while not d(text=u'登录').exists:
@@ -93,16 +130,15 @@ def loginOne(account, password):
             d(text=u'否').click.wait() # 不使用通讯录（不是每次都有）
         time.sleep(0.1)
     time.sleep(2)
-    os.system('adb shell am force-stop com.tencent.mm HERE')
-    os.system('adb shell mv /data/data/com.tencent.mm /data/data/com.tencent.mm.'+account)
-    os.system('adb shell mv /mnt/sdcard/tencent /mnt/sdcard/tencent.'+account)
+    backup(account)
+    log('login ' + account + ' and backup success')
 
 
 # 确保所有的账号都登录了。只提前做一次
 def loginAll():
     # os.system('adb shell pm clear com.tencent.mm HERE')
     for (n, p) in accounts.items():
-        loginOne(n, p)
+        login(n, p)
     log('login success...')
 
 def doVote1():
@@ -212,19 +248,6 @@ def doVote2():
     while not d(text=u'发送').exists:
         time.sleep(0.1)
     d(text=u'发送').click.wait() # 点击“发送”
-
-def restore(account):
-    log('restore '+account)
-    # 移动号码的目录到/data/data/com.tencent.mm
-    os.system('adb shell am force-stop com.tencent.mm HERE')
-    os.system('adb shell mv /data/data/com.tencent.mm.'+account+' /data/data/com.tencent.mm')
-    os.system('adb shell mv /mnt/sdcard/tencent.'+account+' /mnt/sdcard/tencent')
-
-def backup(account):
-    log('backup '+account)
-    os.system('adb shell am force-stop com.tencent.mm HERE')
-    os.system('adb shell mv /data/data/com.tencent.mm /data/data/com.tencent.mm.'+account)
-    os.system('adb shell mv /mnt/sdcard/tencent /mnt/sdcard/tencent.'+account)
 
 def vote(account, password):
     log('vote ' + account + '...')
