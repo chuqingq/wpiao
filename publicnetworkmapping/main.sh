e28527af9e1d11dcdc2862b275c965046f9ca607
chuqingq <chuqingq@qq.com>
2017-03-19 22:20:48 +0800

    public network mapping

---
+++ publicnetworkmapping/main.sh
@@ -0,0 +1,9 @@
+# 先建立本机和服务器的信任关系，然后执行此脚本，确保公网映射一直工作。 
+
+while True
+do
+    echo `date` "will try reconnect in 2 seconds..."
+    ssh -R 0.0.0.0:8080:192.168.31.72:8080 root@121.41.103.23
+    sleep 2 
+done
+
