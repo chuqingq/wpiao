0b1c2490e352b5a7bd7f0303ced7657179234424
chuqingq <chuqingq@qq.com>
2017-03-26 22:40:31 +0800

    delete useless code and comments

--- browser/browser.go
+++ browser/browser.go
@@ -4,23 +4,18 @@ package main
 
 import (
 	"flag"
-	// "io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
 )
 
-// const FILE_NAME = "D:\\abc.txt"
-
 func init() {
 	file, err := os.OpenFile("browser.log", os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0755)
 	if err != nil {
 		log.Fatalf("open voter.log error: %v", err)
 	}
-	// if err := f.Close(); err != nil {
-	// 	log.Fatal(err)
-	// }
+
 	log.SetOutput(file)
 	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds | log.Lshortfile | log.LstdFlags)
 }
@@ -32,16 +27,6 @@ func main() {
 		return
 	}
 
-	// var content []byte
-	// var err error
-	// content, err = ioutil.ReadFile(FILE_NAME)
-	// if err != nil {
-	// 	content = make([]byte, 0)
-	// }
-
-	// str := string(content) + voteUrl + "\r\n"
-
-	// ioutil.WriteFile(FILE_NAME, []byte(str), 0666)
 	log.Printf("voteUrl: %v", voteUrl)
 
 	// 请求通过http发送给
